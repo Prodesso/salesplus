9fa45896fd81aaef38d90771e455d49f4ef95dc5
Prodesso <101436290+Prodesso@users.noreply.github.com>
2023-05-08 21:44:03 +0000

    v3

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(express.static('public'));
 
 // sendFile will go here
-app.get('/:page', function(req, res) {
-  let page = "/html/" + req.params.page + ".html"
+app.get('/dashboard', function(req, res) {
+  let page = "/html/dashboard.html"
   res.sendFile(path.join(__dirname, page));
 });
 
